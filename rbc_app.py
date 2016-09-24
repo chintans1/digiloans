ed4ab04abc01c04561037cf1b9638286e1eb052c
Chintan Shah <cgintan1998@gmail.com>
2016-09-24 17:26:07 -0400

    Fix Href links on index.html

--- rbc_app.py
+++ rbc_app.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template("testcommand.html")
+    return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
 
-    text = request.form['teext']
+    text = request.form['text']
     processed_text = text.upper()
     return processed_text
-
-
-
 
 if __name__ == '__main__':
     app.run()
