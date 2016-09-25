1563ef9220498526688d5b7d5081598b3e537f3a
ViperXYZ <sammy.haque@mail.utoronto.ca>
2016-09-24 23:57:13 -0400

    Added clickable links

--- rbc_app.py
+++ rbc_app.py
@@ -12,9 +12,19 @@ def my_form_post():
     user = request.form['user']
     passw = request.form['pass']
     usr_pass = {"jane":"1234","Mary":"hell0","rndm":"smh"}
-    if((user in usr_pass.keys())and (passw == usr_pass.get(user))):
-    	return render_template("404.html")
-    else:
-        return render_template("sign-up.html")
+    if(request.method == "POST"):
+	    if((user in usr_pass.keys())and (passw == usr_pass.get(user))):
+	    	return render_template("404.html")
+	    else:
+	        return render_template("sign-up.html")
+	else:
+		return render_template('index.html')
+
+
+@app.route('/page')
+def get_page():
+    return send_file('templates/bar.html')
+
+
 if __name__ == '__main__':
     app.run()
