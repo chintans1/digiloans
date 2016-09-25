b2cd56a6bb41ef10c982b2d867bc8f51cbf34e1c
ViperXYZ <sammy.haque@mail.utoronto.ca>
2016-09-25 03:04:53 -0400

    stuff

--- rbc_app.py
+++ rbc_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request,url_for
 
 
 app = Flask(__name__)
+usr_pass = {usr_pass = {"jane":"1234","Mary":"hell0","rndm":"smh"}
 
 @app.route('/')
 def my_form():
@@ -11,7 +12,7 @@ def my_form():
 def my_form_post():
     user = request.form['user']
     passw = request.form['pass']
-    usr_pass = {"jane":"1234","Mary":"hell0","rndm":"smh"}
+    
     if(request.method == "POST"):
 	    if((user in usr_pass.keys())and (passw == usr_pass.get(user))):
 	    	return render_template("404.html")
@@ -20,11 +21,21 @@ def my_form_post():
 	else:
 		return render_template('index.html')
 
-
-@app.route('/page')
-def get_page():
-    return send_file('templates/bar.html')
-
+app.route('/sign-up', methods=['POST'])
+def my_form_post():
+	try:
+	    user = request.form['user']
+	    email = request.form['email']
+	    passw = request.form['pass']
+	    sinnum = request.form['sinnum']
+	    phnnum = request.form['phnnum']
+	    firstname = request.form['fname']
+	    lastname = request.form['lname']
+	    if(request.method == "POST"):
+	    	usr_pass[user] = [email.strip(),passw.strip(),sinnum.strip(),phnnum.strip(),firstname.strip(),lastname.strip()]
+		    	return render_template("user.html")
+		else:
+			return render_template('sign-up.html')
 
 if __name__ == '__main__':
     app.run()
